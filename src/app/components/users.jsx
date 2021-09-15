a53bb9934897c756ebd2cff65ae6e5ef36c864f3
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-09-15 20:08:30 +0300

    fast-company onSort_UsersTable_16

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
-import User from "./user";
 import api from "../api";
 import GroupList from "./groupList";
 import SearchStatus from "./searchStatus";
+import UsersTable from "./usersTable";
 import _ from "lodash";
 
 const Users = ({ users: allUsers, ...rest }) => {
@@ -29,6 +29,10 @@ const Users = ({ users: allUsers, ...rest }) => {
 
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex);
+    };
+
+    const handleSort = (item) => {
+        console.log("item:", item);
     };
 
     const filteredUsers = selectedProf
@@ -60,24 +64,11 @@ const Users = ({ users: allUsers, ...rest }) => {
             <div className="d-flex flex-column">
                 <SearchStatus length={count} />
                 {count > 0 && (
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Имя</th>
-                                <th scope="col">Качества</th>
-                                <th scope="col">Провфессия</th>
-                                <th scope="col">Встретился, раз</th>
-                                <th scope="col">Оценка</th>
-                                <th scope="col">Избранное</th>
-                                <th />
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {usersCrop.map((user) => (
-                                <User {...rest} {...user} key={user._id} />
-                            ))}
-                        </tbody>
-                    </table>
+                    <UsersTable
+                        users={usersCrop}
+                        onSort={handleSort}
+                        {...rest}
+                    />
                 )}
                 <div className="d-flex justify-content-center">
                     <Pagination
