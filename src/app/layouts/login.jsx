34e3a7038c18d910f95aa2011a4a43e02590670d
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-10-06 20:13:16 +0300

    fast-company form customization_26

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    const [email] = useState();
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" value={email} />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" />
+            </div>
+        </form>
+    );
 };
 
 export default Login;
