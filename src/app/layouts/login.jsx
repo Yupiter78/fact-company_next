57d216b8a5d25809cc968bd40c7be1c2d4cf201d
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-10-07 19:18:18 +0300

    fast-company processing the values of plenty fields_28

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
+    const [data, setData] = useState({ email: "", password: "" });
     const handleChange = ({ target }) => {
-        setEmail(target.value);
-        console.log("event.target.value:", target.value);
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
     };
     return (
         <form action="">
@@ -13,13 +15,20 @@ const Login = () => {
                 <input
                     type="text"
                     id="email"
-                    value={email}
+                    name="email"
+                    value={data.email}
                     onChange={handleChange}
                 />
             </div>
             <div>
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                />
             </div>
         </form>
     );
