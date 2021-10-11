57bd1fe581113ca2d45faee44fe9ed28c0643158
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-10-11 23:52:07 +0300

    fast-company error display_31

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -3,7 +3,7 @@ import TextField from "../components/textField";
 
 const Login = () => {
     const [data, setData] = useState({ email: "", password: "" });
-    const [, setErrors] = useState();
+    const [errors, setErrors] = useState({});
     const handleChange = ({ target }) => {
         setData((prevState) => ({
             ...prevState,
@@ -36,6 +36,7 @@ const Login = () => {
                 name="email"
                 value={data.email}
                 onChange={handleChange}
+                error={errors.email}
             />
             <TextField
                 label="Password"
@@ -43,6 +44,7 @@ const Login = () => {
                 name="password"
                 value={data.password}
                 onChange={handleChange}
+                error={errors.password}
             />
             <button type="submit">Submit</button>
         </form>
