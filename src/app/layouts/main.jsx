680913e676e80f5f2d29ff4f644dc1431d34c5af
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-11-20 01:29:30 +0300

    fast-company mock_data_initialization_part_2__59

--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import useMockData from "../utils/mockData";
 
 const Main = () => {
-    const { initialize } = useMockData();
+    const { error, initialize, status, progress } = useMockData();
     const handleClick = () => {
         console.log("clicked");
         initialize();
@@ -11,6 +11,11 @@ const Main = () => {
         <div className="container mt-5">
             <h1> Main Page</h1>
             <h3>initializing data in Firebase</h3>
+            <ul>
+                <li>Status: {status}</li>
+                <li>Progress: {progress}</li>
+                {error && <li>{error}</li>}
+            </ul>
             <button className="btn btn-primary" onClick={handleClick}>
                 initialize
             </button>
