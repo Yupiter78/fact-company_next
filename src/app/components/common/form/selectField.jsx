99c842564d6f94ad28fb8cfea6db79432a46b063
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-11-27 15:46:33 +0300

    fast-company AuthProvider__62

--- src/app/components/common/form/selectField.jsx
+++ src/app/components/common/form/selectField.jsx
@@ -10,7 +10,6 @@ const SelectField = ({
     error,
     name
 }) => {
-    console.log("option:", options);
     const handleChange = ({ target }) => {
         onChange({ name: target.name, value: target.value });
     };
