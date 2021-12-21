3ef068ad5133149f649c8740bc448321900bc28b
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-12-22 01:30:38 +0300

    fast-company add_user_to_navbar__78

--- src/app/components/ui/navProfile.jsx
+++ src/app/components/ui/navProfile.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { Link } from "react-router-dom";
 
 const NavProfile = () => {
-    return <h1>User</h1>;
+    const { currentUser } = useAuth();
+    const [isOpen, setOpen] = useState(false);
+    const toggleMenu = () => {
+        setOpen((prevState) => !prevState);
+    };
+    return (
+        <div className="dropdown" onClick={toggleMenu}>
+            <div className="bnt dropdown-toggle d-flex align-items-center">
+                <div className="me-2">{currentUser.name}</div>
+                <img
+                    src={`https://avatars.dicebear.com/api/avataaars/${(
+                        Math.random() + 1
+                    )
+                        .toString(36)
+                        .substring(7)}.svg`}
+                    alt=""
+                    height="40"
+                    className="img-responsive rounded-circle"
+                />
+            </div>
+            <div className={"w-100 dropdown-menu" + (isOpen ? "show" : "")}>
+                <Link
+                    to={`/users/${currentUser._id}`}
+                    className="dropdown-item"
+                >
+                    Profile
+                </Link>
+                <Link to="logout" className="dropdown-item">
+                    Log Out
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default NavProfile;
