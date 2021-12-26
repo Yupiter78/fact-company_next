a6ef36b5b73ea6cd9928403a64c310753613ac98
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-12-26 16:36:13 +0300

    fast-company delete_comments__92

--- src/app/hooks/useComments.jsx
+++ src/app/hooks/useComments.jsx
@@ -54,6 +54,19 @@ export const CommentsProvider = ({ children }) => {
         setError(message);
     }
 
+    async function removeComment(id) {
+        try {
+            const { content } = await commentService.removeComment(id);
+            if (content === null) {
+                setComments((prevState) =>
+                    prevState.filter((comment) => comment._id !== id)
+                );
+            }
+        } catch (error) {
+            errorCatcher(error);
+        }
+    }
+
     useEffect(() => {
         if (error !== null) {
             toast(error);
@@ -63,7 +76,7 @@ export const CommentsProvider = ({ children }) => {
 
     return (
         <CommentsContext.Provider
-            value={{ comments, createComment, isLoading }}
+            value={{ comments, createComment, isLoading, removeComment }}
         >
             {children}
         </CommentsContext.Provider>
