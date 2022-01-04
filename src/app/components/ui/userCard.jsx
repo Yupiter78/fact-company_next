697698edf711ccaedb9b983ad2a7d2f6dab293ed
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-01-05 01:27:18 +0300

    fast-company refactoring_UserCard__100

--- src/app/components/ui/userCard.jsx
+++ src/app/components/ui/userCard.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import { useProfessions } from "../../hooks/useProfession";
 
 const UserCard = ({ user }) => {
     const history = useHistory();
     const { currentUser } = useAuth();
+    const { getProfession, isLoading: professionIsLoading } = useProfessions();
     const handleClick = () => {
         history.push(history.location.pathname + "/edit");
     };
@@ -30,7 +32,8 @@ const UserCard = ({ user }) => {
                     <div className="mt-3">
                         <h4>{user.name}</h4>
                         <p className="text-secondary mb-1">
-                            {user.profession.name}
+                            {!professionIsLoading &&
+                                getProfession(user.profession)?.name}
                         </p>
                         <div className="text-muted">
                             <i
