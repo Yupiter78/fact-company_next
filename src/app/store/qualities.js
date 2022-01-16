922e5e2ab5cabcd89cb0458e5a125dabe06bd629
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-01-16 23:59:11 +0300

    fast-company__creating_a_qualities_reducer__104

--- src/app/store/qualities.js
+++ src/app/store/qualities.js
@@ -4,10 +4,29 @@ const qualitiesSlice = createSlice({
     name: "qualities",
     initialState: {
         entities: null,
-        isLoading: true
+        isLoading: true,
+        error: null
+    },
+    reducers: {
+        qualitiesRequested: (state) => {
+            state.isLoading = true;
+        },
+        qualitiesReceived: (state, action) => {
+            state.entities = action.payload;
+            state.isLoading = false;
+        },
+        qualitiesRequestFiled: (state, action) => {
+            state.error = action.payload;
+            state.isLoading = false;
+        }
     }
 });
 
-const { reducer: qualitiesReducer } = qualitiesSlice;
+const { reducer: qualitiesReducer, actions } = qualitiesSlice;
+const { qualitiesRequested, qualitiesReceived, qualitiesRequestFiled } =
+    actions;
+const loadQualitiesList = () => (dispatch) => {
+    dispatch(qualitiesReceived());
+};
 
 export default qualitiesReducer;
